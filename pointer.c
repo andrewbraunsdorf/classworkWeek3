ebda100154091c35359016e504b6386175129882
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-05-21 22:39:00 +0000

    made a pointer

--- pointer.c
+++ pointer.c
@@ -5,22 +5,24 @@ int add(int x, int y)
 {
     return x + y;
 }
-int modifyAdder(int x, int y)
+int modifyAdder(int *x, int y)
 {
-    x+= 7;
-    return x + y;
+    *x+= 7;
+    return *x + y;
 }
 
 int main (void)
 {
     int a = 5;
     int b = 13;
-    // printf("The total is %i!\n", add(a, b));
-    // printf("The modified total is %i!\n", modifyAdder(a, b));
-    // printf("Running normal add again: %i!\n", add(a,b));
+    printf("The total is %i!\n", add(a, b));
+    printf("The modified total is %i!\n", modifyAdder(&a, b));
+    printf("Running normal add again: %i!\n", add(a,b));
+    printf("The modified again is %i!\n", modifyAdder(&a, b));
+    printf("Running normal add a second time: %i!\n", add(a, b));
 
-    int* intPointer = &b;
-    printf("b is %i\n", b);
-    *intPointer += 4;
-    printf("now b is %i\n", b);
+    // int* intPointer = &b;
+    // printf("b is %i\n", b);
+    // *intPointer += 4;
+    // printf("now b is %i\n", b);
 }
