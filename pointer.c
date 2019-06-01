631ec996dedb208fef9fe18a369664cf960ad507
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-06-01 16:47:54 +0000

    changed addNode to reference pointers and next

--- pointer.c
+++ pointer.c
@@ -24,12 +24,36 @@ int modifyAdder(int *x, int y)
     return *x + y;
 }
 
+void addNode(int value, NODE *homeNode)
+{
+    NODE *newNode = malloc(sizeof(NODE));
+    newNode->value = value;
+    homeNode->next = newNode;
+}
+
+// // this was referencing an empty part of the cycle where it was not grabbing anything
+// void addNode(int value, NODE *homeNode)
+// {
+//     NODE newNode;
+//     newNode.value = value;
+//     homeNode->next = &newNode;
+// }
+
 int main (void)
 {
     NODE rootNode;
     rootNode.value = 5;
+    rootNode.next = NULL;
+
+    addNode(10, &rootNode);
 
     printf("%i\n", rootNode.value);
+    printf("%i\n", rootNode.next->value);
+
+    // NODE secondNode;
+    // secondNode.value = 10;
+    // rootNode.next = &secondNode;
+
     // int a = 5;
     // int b = 13;
     // printf("The total is %i!\n", add(a, b));
