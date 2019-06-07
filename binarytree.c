16a6e53d99db457b970eb6bd2a0c595015d3383f
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-06-07 21:56:42 +0000

    added left/right nodes

--- binarytree.c
+++ binarytree.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-\\ minichallenge create the NODE for a binary tree.
+// minichallenge create the NODE for a binary tree.
 
 typedef struct NODE
 {
@@ -10,7 +10,34 @@ typedef struct NODE
     struct NODE *left;
     struct NODE *right;
 } NODE;
+
+NODE *rootNode;
+
+void addNode(int value)
+{
+    NODE *newNode = malloc(sizeof(NODE));
+    newNode->value = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    if(rootNode == NULL)
+    {
+        rootNode = newNode;
+        return;
+    }
+    if (rootNode->value < value)
+    {
+        rootNode->left = newNode;
+    }
+    else
+    {
+        rootNode->right = newNode;
+    }
+}
 int main(void)
 {
     printf("hello\n");
+    addNode(10);
+    addNode(8);
+    addNode(15);
 }
